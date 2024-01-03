83de21ec895b7e89146fc7735b4c99634618fa5a
Maha A S A Salem <83347@kfh.com>
2024-01-03 10:29:13 +0300

    Array task

---
+++ Arrays.java
@@ -0,0 +1,20 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+class Main {
+    public static void main(String[] args) {
+  
+      int[] tempreture = {10,20,30,40,50};
+      tempreture [2]=10;
+  
+      for (int temp : tempreture){
+        System.out.println(temp);
+      }
+  
+  
+    }
+  
+  
+    // @Test
+    // void addition() {
---
+++ Arrays/Main.java
@@ -0,0 +1,23 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+class Main {
+  public static void main(String[] args) {
+
+    int[] tempreture = {10,20,30,40,50};
+    tempreture [2]=10;
+
+    for (int temp : tempreture){
+      System.out.println(temp);
+    }
+
+
+  }
+
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
